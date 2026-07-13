afcc4b7db2b9de100e06c7cb750657ec80f05c2b
Aymer Yesid Cruz Suescun <87399535+AymerSu@users.noreply.github.com>
2026-07-13 15:19:09 -0500

    iter01: :package: config sonar

--- jest.config.js
+++ jest.config.js
@@ -8,14 +8,5 @@ module.exports = {
     'text',
     'html',
     'lcov'
-  ],
-
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  }
+  ]
 };
